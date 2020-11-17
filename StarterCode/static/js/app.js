b01e47fdc1f8c2003c2d6f942340333766fd5680
Kimberly Flores <kimberlyflores@Kimberlys-MBP.localdomain>
2020-11-16 19:53:27 -0600

    populating the drop down

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,23 +1,39 @@
 
 d3.json("samples.json").then((importedData) => {
-    // console.log(importedData);
-   //var data = importedData;
+    //importing the data from JSON format
     var data = importedData;
-
+    //saving the names for the dropdown
     var dropDown = data.names;
+    //saving the info of the patitents
+    var metadata = data.metadata;
+    // saving the samples of the data
+    var samples = data.samples;
+
 
     console.log(dropDown);
-
+    // populate drop-down
     d3.select("#selDataset")
         .selectAll("option")
         .data(dropDown)
         .enter()
         .append("option")
-        .attr("value", function(option) { return option.value; })
-        .text(function(option) { return option.text; });
-});
+        .attr("value", function (data, index) {
+            return data;
+        })
+        .text(function (data, index) {
+            return data;
+        });
 
-//var selectDropDown = 
+
+
+    
+
+
+
+
+    });
+
+
 
 
 
