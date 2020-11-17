66cec2c58fc11b24f78d51f790a025fc5eec0373
Kimberly Flores <kimberlyflores@Kimberlys-MBP.localdomain>
2020-11-16 21:09:42 -0600

    adding in different functions

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,15 +1,13 @@
-
+//on load, the page will load the drop down
+d3.select(window).on("load", loadDropDown);
+//function to load the data
+function loadDropDown(){
 d3.json("samples.json").then((importedData) => {
     //importing the data from JSON format
     var data = importedData;
     //saving the names for the dropdown
     var dropDown = data.names;
-    //saving the info of the patitents
-    var metadata = data.metadata;
-    // saving the samples of the data
-    var samples = data.samples;
-
-
+    
     console.log(dropDown);
     // populate drop-down
     d3.select("#selDataset")
@@ -24,14 +22,42 @@ d3.json("samples.json").then((importedData) => {
             return data;
         });
 
+    });
+};
 
+//when the dropdown changes load the information
+d3.select('#selDataset').on('change', updateInfo);
 
-    
+function updateInfo(){
+    //preventing from not reloading
+    d3.event.preventDefault();
+    //grabbing the data to load
+    //grabbing what the user selected
+    var dropdown = d3.select('#selDataset');
+    var dropdownValue = dropdown.property('value');
+    console.log(dropdownValue)
+    d3.json("samples.json").then((importedData) => {
+        //importing the data from JSON format
+        var data = importedData;
+        //saving the info of the patitents
+        var metadata = data.metadata;
+        // saving the samples of the data
+        var samples = data.samples;
 
+        //Now to fill the demographic data
+        data_info = []
+        var data_info = metadata.filter(function(d){
+            if d.id === dropdownValue{
+                return d
+            };
+        });
 
+        console.log(data_info)
 
 
     });
+
+}
 
 
 
